3838e65a5017b5fb4cc00facf42ef9e4a4f6e46c
Schoofs Ebert <ebertschoofs@gmail.com>
2020-04-10 14:27:51 +0200

    Made use of motifs library

--- sampler.py
+++ sampler.py
@@ -1,6 +1,7 @@
 import sys
 import random
-from Bio import SeqIO, motifs
+from Bio import SeqIO, motifs, Seq
+from Bio.motifs import thresholds, matrix
 import math
 import random
 import copy
@@ -16,86 +17,70 @@ def main(filename, motiflength):
     instanceref = get_random_instances(records, motiflength)
     print("found %d sequences" % len(instanceref))
     print("Got random instances:")
-    for instance in instanceref:
-        print(instance)
-    profile = create_profile(instanceref)
+    motif = motifs.create(instanceref)
+    print(motif)
     print("Starting profile")
-    for nucleotide in nucleotides:
-        print(nucleotide + "\t\t", end='')
-        for instance in profile:
-            # this special print syntax is to allign the printing
-            print("{:>12} \t".format(str(profile[instance][nucleotide])), end='')
-        # print a newline
-        print("")
-    recursive_random(instanceref, motiflength, records)
+    prof = create_pssm(motif)
+    print(prof)
+    motif = recursive_random(instanceref, motiflength, records)
+    print("Creating results.png")
+    motif.weblogo("results.png")
 
 
-def create_profile(instances):
-    pseudocount = 1
-    instance_dict = {}
-    for i in range(len(instances[0])):
-        instance_dict[i] = {}
-        instance_dict[i]["all"] = 0
-        for nucleotide in nucleotides:
-            instance_dict[i][nucleotide] = pseudocount
-            instance_dict[i]["all"] += 1
-        for seq in instances:
-            instance_dict[i][seq[i]] += 1
-            instance_dict[i]["all"] += 1
-        for nucleotide in nucleotides:
-            # We make a weighted calculation to get the profile numbers
-            instance_dict[i][nucleotide] = -math.log(instance_dict[i][nucleotide] / instance_dict[i]["all"]);
-            instance_dict[i][nucleotide] = int(instance_dict[i][nucleotide] * 100) / 100
-    return instance_dict
+def create_pssm(instances):
+    profile = instances.counts.normalize(pseudocounts=1)
+    for nct in nucleotides:
+        tup = []
+        for i in range(len(profile[nct])):
+            tup.append(-math.log(profile[nct][i]))
+        profile[nct] = tuple(tup)
+    return matrix.PositionSpecificScoringMatrix(alphabet=nucleotides,values=profile)
 
 
-def recursive_random(instances, motiflength, sequences):
+def recursive_random(instances, motiflength, records):
     old_total = check_solution(instances)
 
     for idx, instance in enumerate(instances):
-        leave_out = random.randint(0, len(instances) - 1)
-        print("Leaving out %s" % instances[leave_out])
-        # making training instances which are a copy of the given instances -1 randomly chosen instance
-        train_instances = list()
-        for train_idx, train_instance in enumerate(instances):
-            if (train_idx != leave_out): train_instances.append(copy.deepcopy(train_instance))
+        train_instances = copy.deepcopy(instances)
+        leave_out = random.choice(train_instances)
+        print("Leaving out %s" % leave_out)
+        train_instances.remove(leave_out)
 
-        profile = create_profile(train_instances)
+        train_motifs = motifs.create(train_instances)
+        profile = create_pssm(train_motifs)
 
-        leftseqs = [sequences[leave_out]]
-
+        leftseqs = [records[instances.index(leave_out)]]
         new_instances = get_best_matches(leftseqs, profile, motiflength)
         print("new best instance:")
         for new_instance in new_instances:
             print(new_instance)
-            instances[leave_out] = new_instance
+            instances[instances.index(leave_out)] = new_instance
     # printing the result from this iteration
     total = check_solution(instances)
-    profile = create_profile(instances)
+    profile = create_pssm(motifs.create(instances))
     print("new solution: %d" % total)
     print("new profile: ")
-    for nucleotide in nucleotides:
-        print(nucleotide + "\t\t", end='')
-        for instance in profile:
-            # this special print syntax is to allign the printing
-            print("{:>12} \t".format(str(profile[instance][nucleotide])), end='')
-        # print a newline
-        print("")
+    print(profile)
     #     Check if there is no regression, if not continue the recursion else stop the program
     if total < old_total:
-        profile=recursive_random(instances,motiflength,sequences)
+        return recursive_random(instances,motiflength,records)
     else:
-        pass
-        # Here something strange is done in the perl script (with the scalar(@instances) and i do not know what it means
-        """print("Final profile")
-        for nucleotide in nucleotides:
-            print(nucleotide + "\t\t", end='')
-            for i in range(motiflength):
-                # this special print syntax is to allign the printing
-                print(str(int(math.exp(-profile[i][nucleotide]))))
-                print("{:>12} \t".format(str(int(math.exp(-profile[i][nucleotide]))*((instances)+4))), end='')
-            # print a newline
-            print("")"""
+        motif = motifs.create(instances)
+        print("Finale Profile")
+        print_pseudo(motif)
+        print("Consensus sequence")
+        print(motif.consensus)
+        return motif
+
+
+def print_pseudo(motif):
+    counts = motif.counts
+    for nct in nucleotides:
+        tup = []
+        for i in range(len(counts[nct])):
+            tup.append(counts[nct][i]+1)
+        counts[nct] = tuple(tup)
+    print(counts)
 
 def get_best_matches(leftseq, profile, motif_length):
     best_matches = list()
@@ -103,8 +88,8 @@ def get_best_matches(leftseq, profile, motif_length):
         best_score = 999
         best_match = ""
         for i in range(len(seq) - 1 - motif_length):
-            dnaseq = str(seq.seq)[i:i + motif_length]
-            score = score_profile(dnaseq, profile)
+            dnaseq = Seq.Seq(str(seq.seq)[i:i + motif_length])
+            score = profile.calculate(dnaseq)
             if (score < best_score):
                 best_score = score
                 best_match = dnaseq
@@ -113,15 +98,11 @@ def get_best_matches(leftseq, profile, motif_length):
 
 
 def check_solution(instances):
-    profile = create_profile(instances)
+    pssm = create_pssm(motifs.create(instances))
     old_scores = list()
     for instance in instances:
-        old_scores.append(score_profile(instance, profile))
-
-    old_total = 0
-    for score in old_scores:
-        old_total += score
-    return old_total
+        old_scores.append(pssm.calculate(instance))
+    return sum(old_scores)
 
 
 def score_profile(seq, profile):
@@ -132,7 +113,7 @@ def score_profile(seq, profile):
 
 
 def get_random_instances(records, motiflength):
-    instances = []
+    instances = motifs.Instances()
     for record in records:
         pos = random.randint(0, len(record.seq) - motiflength)
         instances.append(record.seq[pos:pos + motiflength])
