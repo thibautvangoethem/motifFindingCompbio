3d0cc764d0123d679db9cf1f3e96f7f51a6e5031
Schoofs Ebert <ebertschoofs@gmail.com>
2020-04-26 11:27:17 +0200

    Added sampler cml argument, compare sequences for pallindrome

--- sampler.py
+++ sampler.py
@@ -2,19 +2,21 @@ import sys
 import random
 from Bio import SeqIO, motifs, Seq
 from Bio.motifs import thresholds, matrix
+from difflib import SequenceMatcher
 import math
 import random
 import copy
 
 nucleotides = ['A', 'C', 'G', 'T']
 random.seed()
+pallindromic = ["-p", "--pallindrome"]
 
 
-def main(filename, motiflength):
+def main(filename, motiflength, pallindrome=False):
     # read in file
     records = list(SeqIO.parse(filename, "fasta"))
     # creat random instances of given motif size
-    instanceref = get_random_instances(records, motiflength)
+    instanceref = get_random_instances(records, motiflength, pallindrome)
     print("found %d sequences" % len(instanceref))
     print("Got random instances:")
     motif = motifs.create(instanceref)
@@ -22,7 +24,7 @@ def main(filename, motiflength):
     print("Starting profile")
     prof = create_pssm(motif)
     print(prof)
-    motif = recursive_random(instanceref, motiflength, records)
+    motif = recursive_random(instanceref, motiflength, records, pallindrome)
     print("Creating results.pdf")
     motif.weblogo("results.pdf", format="pdf", show_errorbars=False,
         show_ends=False, color_scheme="color_classic")
@@ -38,7 +40,7 @@ def create_pssm(instances):
     return matrix.PositionSpecificScoringMatrix(alphabet=nucleotides,values=profile)
 
 
-def recursive_random(instances, motiflength, records):
+def recursive_random(instances, motiflength, records, pallindrome=False):
     old_total = check_solution(instances)
 
     for idx, instance in enumerate(instances):
@@ -64,13 +66,16 @@ def recursive_random(instances, motiflength, records):
     print(profile)
     #     Check if there is no regression, if not continue the recursion else stop the program
     if total < old_total:
-        return recursive_random(instances,motiflength,records)
+        return recursive_random(instances,motiflength,records, pallindrome)
     else:
         motif = motifs.create(instances)
         print("Finale Profile")
         print_pseudo(motif)
         print("Consensus sequence")
-        print(motif.consensus)
+        if pallindrome:
+            print(motif.consensus + "----" + motif.consensus.reverse_complement())
+        else:
+            print(motif.consensus)
         return motif
 
 
@@ -106,23 +111,43 @@ def check_solution(instances):
     return sum(old_scores)
 
 
-def get_random_instances(records, motiflength):
+def get_random_instances(records, motiflength, pallindrome=False):
     instances = motifs.Instances()
     for record in records:
-        pos = random.randint(0, len(record.seq) - motiflength)
-        instances.append(record.seq[pos:pos + motiflength])
+        if pallindrome:
+            found = False
+            while not found:
+                pos = random.randint(0, len(record.seq) - motiflength)
+                seq = record.seq[pos:pos + motiflength]
+                rev_compl = seq.reverse_complement()
+                if SequenceMatcher(None, seq, rev_compl).ratio() > 0.7:
+                    instances.append(seq)
+                    found = True
+        else:
+            pos = random.randint(0, len(record.seq) - motiflength)
+            seq = record.seq[pos:pos + motiflength]
+            instances.append(seq)
     return instances
 
 
 if __name__ == "__main__":
     filename = ""
     motiflength = 0
-    if len(sys.argv) != 3:
-        print("usage: sampler.py [filename] [motiflength]")
+    pal = False
+    if len(sys.argv) < 3:
+        print("usage: sampler.py [option] [filename] [motiflength]")
+        print("\t -p\t --pallindrome\t Try to find pallindromic motif")
     else:
         try:
-            filename = sys.argv[1]
-            motiflength = int(sys.argv[2])
+            if len(sys.argv) == 4:
+                if sys.argv[1] in pallindromic:
+                    pal = True
+                    filename = sys.argv[2]
+                    motiflength = int(sys.argv[3])
+            else:
+                filename = sys.argv[1]
+                motiflength = int(sys.argv[2])
         except:
-            print("usage: sampler.py [filename] [motiflength]")
-    main(filename, motiflength)
+            print("usage: sampler.py [option] [filename] [motiflength]")
+            print("\t -p\t --pallindrome\t Try to find pallindromic motif")
+    main(filename, motiflength, pallindrome=pal)
