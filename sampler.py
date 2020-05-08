59d32df7af985a8ecc0de65c8bb3a63049a918e9
Schoofs Ebert <ebertschoofs@gmail.com>
2020-05-08 10:49:56 +0200

    Fixed pallindrome check

--- sampler.py
+++ sampler.py
@@ -165,13 +165,20 @@ def get_best_matches(leftseq, profile, motif_length, idx):
                     # This long line is for getting a sequence with a given gap in it
                     dnaseq = Seq.Seq(
                         str(seq.seq)[i:i + j] + str(seq.seq)[i + j + loop_gapsize:i + motif_length + loop_gapsize])
-                    rev_compl = dnaseq.reverse_complement()
-                    if Config.palindrome_enable and jaro_distance(str(dnaseq), str(rev_compl)) > Config.palindrome:
+                    if Config.palindrome_enable:
+                        rev_compl = dnaseq.reverse_complement()
+                        if jaro_distance(str(dnaseq), str(rev_compl)) > Config.palindrome:
+                            score = profile.calculate(dnaseq)
+                            if score < best_score:
+                                best_score = score
+                                inner_Gaplist[idx] = j
+                                best_match = dnaseq
+                    else:
                         score = profile.calculate(dnaseq)
-                        if (score < best_score):
+                        if score < best_score:
                             best_score = score
                             inner_Gaplist[idx] = j
-                            best_match = (dnaseq)
+                            best_match = dnaseq
             if best_match == "":
                 exit("No sequence found with a high enough palindromic ratio, try lowering it")
 
